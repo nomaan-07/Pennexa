d005bb221921b04aeb27ee849abed1a7c00ae2df
nomaan-07 <nomaan07.dev@gmail.com>
2025-03-22 13:19:39 +0330

    feat(router): add React Router with initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import Dashboard from './pages/Dashboard';
+import Expenses from './pages/Expenses';
+import Income from './pages/Income';
+import Groups from './pages/Groups';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import ErrorPage from './pages/ErrorPage';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: 'expenses',
+        element: <Expenses />,
+      },
+      {
+        path: 'income',
+        element: <Income />,
+      },
+      {
+        path: 'groups',
+        element: <Groups />,
+      },
+      {
+        path: 'account',
+        element: <Account />,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
