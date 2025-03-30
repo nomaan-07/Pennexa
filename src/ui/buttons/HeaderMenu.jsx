70f47e1ea37a8713262eaab09194435666c7c925
nomaan-07 <nomaan07.dev@gmail.com>
2025-03-31 01:51:29 +0330

    fix: add user profile to mobile heaedr menu

--- src/ui/buttons/HeaderMenu.jsx
+++ src/ui/buttons/HeaderMenu.jsx
@@ -2,11 +2,12 @@ import AddTransaction from "./AddTransaction";
 import DarkModeToggle from "./DarkModeToggle";
 import Logout from "../../features/authentication/Logout";
 import TopSheet from "../common/TopSheet";
+import UserProfile from "../../features/authentication/UserProfile";
 
 import { Menu } from "lucide-react";
 import { useModal } from "../../hooks/uesModal";
 
-function HeaderButtons() {
+function HeaderMenu() {
   const { openModal, closeModal, isOpen } = useModal();
 
   return (
@@ -18,6 +19,9 @@ function HeaderButtons() {
       <Menu className="size-7 lg:hidden" onClick={openModal} />
       <TopSheet isOpen={isOpen} onClose={closeModal} closeButtonLocation="left">
         <div className="flex flex-col items-end gap-6 bg-white transition-colors lg:hidden dark:bg-slate-800">
+          <div className="mx-auto">
+            <UserProfile />
+          </div>
           <AddTransaction />
           <DarkModeToggle />
           <Logout />
@@ -27,4 +31,4 @@ function HeaderButtons() {
   );
 }
 
-export default HeaderButtons;
+export default HeaderMenu;
--- src/ui/navigation/Header.jsx
+++ src/ui/navigation/Header.jsx
@@ -1,4 +1,4 @@
-import HeaderButtons from "../buttons/HeaderButtons";
+import HeaderMenu from "../buttons/HeaderMenu";
 import HeaderTile from "./HeaderTile";
 import Logo from "./Logo";
 
@@ -7,7 +7,7 @@ function Header() {
     <header className="fixed top-0 right-0 left-0 z-60 flex h-14 items-center justify-between bg-white px-4 sm:px-8 lg:left-70 lg:h-20 lg:px-12 lg:py-4 dark:bg-slate-800">
       <Logo type="header" className="lg:hidden" />
       <HeaderTile />
-      <HeaderButtons />
+      <HeaderMenu />
     </header>
   );
 }
