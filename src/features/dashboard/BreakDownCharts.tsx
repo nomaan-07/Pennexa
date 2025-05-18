0353ae7a6ca039e89043057be922c56934d02acd
nomaan-07 <nomaan07.dev@gmail.com>
2025-05-18 23:30:45 +0330

    refactor(BrekDownCharts): convert to TypeScript with typed props

--- src/features/dashboard/BreakDownCharts.tsx
+++ src/features/dashboard/BreakDownCharts.tsx
@@ -1,6 +1,11 @@
+import { Transaction } from "../../utils/types";
 import BreakDownChart from "./BreakDownChart";
 
-function BreakDownCharts({ incomes, expenses }) {
+interface BreakDownChartsProps {
+  incomes: Transaction[];
+  expenses: Transaction[];
+}
+function BreakDownCharts({ incomes, expenses }: BreakDownChartsProps) {
   return (
     <div className="xs:gap-6 grid gap-4 md:grid-cols-2">
       <BreakDownChart type="income" data={incomes} />
