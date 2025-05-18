1f2b17b0f9e22d118d1982b9097fde88c546846e
nomaan-07 <nomaan07.dev@gmail.com>
2025-05-18 19:49:17 +0330

    refactor(useQueryParam): convert to TypeScript with typed props

--- src/hooks/useQueryParam.ts
+++ src/hooks/useQueryParam.ts
@@ -1,19 +1,32 @@
 import { useSearchParams } from "react-router";
 
+type Field = "page" | "last" | "sortBy";
+type DefaultValue = "all" | "date-desc" | 1;
+type Option = {
+  value: string;
+  label: string;
+};
+
+interface Updates {
+  last?: string;
+  sortBy?: string;
+  page: string;
+}
+
 export function useQueryParam() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  function getQueryParam(field, defaultValue) {
+  function getQueryParam(field: Field, defaultValue: DefaultValue) {
     return searchParams.get(field) || defaultValue;
   }
 
-  function setQueryParam(field, value) {
+  function setQueryParam(field: Field, value: string) {
     const newParams = new URLSearchParams(searchParams);
     newParams.set(field, value);
     setSearchParams(newParams);
   }
 
-  function setQueryParams(updates) {
+  function setQueryParams(updates: Updates) {
     const newParams = new URLSearchParams(searchParams);
     Object.entries(updates).forEach(([field, value]) => {
       newParams.set(field, value);
@@ -21,8 +34,8 @@ export function useQueryParam() {
     setSearchParams(newParams);
   }
 
-  function getCurrentQueryParam(field, options) {
-    return searchParams.get(field) || options.at(0).value;
+  function getCurrentQueryParam(field: Field, options: Option[]) {
+    return searchParams.get(field) || options[0].value;
   }
 
   return { getQueryParam, setQueryParam, getCurrentQueryParam, setQueryParams };
